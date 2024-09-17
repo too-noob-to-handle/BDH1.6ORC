dc99c6b8511c77c4ffb6f7f9e6e90f3c0b2d2c06
too-noob-to-handle <98410308+too-noob-to-handle@users.noreply.github.com>
2024-09-17 18:00:35 +0600

    Create deepto.py

---
+++ deepto.py
@@ -0,0 +1,74 @@
+#@title Deepto DL 1080p & 720p
+
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import shutil
+import glob
+import pathlib
+import platform
+import time
+
+# Paths
+FILE_DIRECTORY=str(pathlib.Path(__file__).parent.absolute())
+TEMPORARY_PATH = FILE_DIRECTORY+"/cache"
+OUTPUT_PATH = FILE_DIRECTORY+"/output"
+UTILS = FILE_DIRECTORY+"/utils"
+TAG = "JoyBangla
+
+def divider():
+    print ('-' * shutil.get_terminal_size().columns)
+
+def empty_folder(folder):
+    files = glob.glob('%s/*'%folder)
+    for f in files:
+        os.remove(f)
+    print("Emptied Temporary Files!")
+    divider()
+    quit()
+
+def download_drm_content(mpd_url):
+    divider()
+    print("Processing Video Info..")
+    #divider()
+    #FILENAME0 = input("ENTER 2160p FileName: ")
+    #divider()
+    FILENAME = input("ENTER 1080p FileName: ")
+    divider()
+    FILENAME2 = input("ENTER 720p FileName: ")
+
+    divider()
+    #print("Downloading 2160p Video from CDN..")
+    #os.system(f'{UTILS}/N_m3u8DL-RE -sv res="2160*" -sa best {mpd_url} -M format=mp4:muxer=ffmpeg --tmp-dir "{TEMPORARY_PATH}" --save-dir "{TEMPORARY_PATH}" --save-name "temp0-BDH" -H "Origin: https://www.deeptoplay.com" -H "Referer: https://www.deeptoplay.com/" -H "User-Agent: Dart/2.18 (dart:io)" --binary-merge --del-after-done --log-level OFF && mkvmerge --output "{OUTPUT_PATH}/{FILENAME0}-{TAG}.mkv" --track-name "0:Join Us - @JoyBangla4U" --track-name "1:Join Us - @JoyBangla4U" \'(\' {TEMPORARY_PATH}/temp0-BDH.mp4 \')\' --track-name "0:Join Us - @JoyBangla4U" \'(\' /content/ZGH596AF1426AF58623AGVH.srt \')\' --title "Join Us - @JoyBangla4U" --track-order 0:0,0:1,1:0 ')
+    time.sleep(2)
+    print("Downloading 1080p Video from CDN..")
+    os.system(f'{UTILS}/N_m3u8DL-RE -sv res="1080*" -sa best {mpd_url} -M format=mp4:muxer=ffmpeg --tmp-dir "{TEMPORARY_PATH}" --save-dir "{TEMPORARY_PATH}" --save-name "temp-BDH" -H "Origin: https://www.deeptoplay.com" -H "Referer: https://www.deeptoplay.com/" -H "User-Agent: Dart/2.18 (dart:io)" --binary-merge --del-after-done --log-level OFF && mkvmerge --output "{OUTPUT_PATH}/{FILENAME}-{TAG}.mkv" --track-name "0:Join Us - @JoyBangla4U" --track-name "1:Join Us - @JoyBangla4U" \'(\' {TEMPORARY_PATH}/temp-BDH.mp4 \')\' --track-name "0:Join Us - @JoyBangla4U" \'(\' /content/ZGH596AF1426AF58623AGVH.srt \')\' --title "Join Us - @JoyBangla4U" --track-order 0:0,0:1,1:0 ')
+    print("Downloading 720p Video from CDN..")
+    time.sleep(2)
+    os.system(f'{UTILS}/N_m3u8DL-RE -sv res="720*" -sa best {mpd_url} -M format=mp4:muxer=ffmpeg --tmp-dir "{TEMPORARY_PATH}" --save-dir "{TEMPORARY_PATH}" --save-name "temp2-BDH" -H "Origin: https://www.deeptoplay.com" -H "Referer: https://www.deeptoplay.com/" -H "User-Agent: Dart/2.18 (dart:io)" --binary-merge --del-after-done --log-level OFF && mkvmerge --output "{OUTPUT_PATH}/{FILENAME2}-{TAG}.mkv" --track-name "0:Join Us - @JoyBangla4U" --track-name "1:Join Us - @JoyBangla4U" \'(\' {TEMPORARY_PATH}/temp-BDH2.mp4 \')\' --track-name "0:Join Us - @JoyBangla4U" \'(\' /content/ZGH596AF1426AF58623AGVH.srt \')\' --title "Join Us - @JoyBangla4U" --track-order 0:0,0:1,1:0 ')
+
+def drive_upload():
+    divider()
+    print("Uploading.. (Takes some time)")
+    time.sleep(2)
+    os.system('rclone --config=/content/accounts/DRMv1.6.AUM.Linux/utils/rclone.conf copy --update --verbose --transfers 30 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s "/usr/src/app/accounts/DRMv1.6.AUM.Linux/output" "BDHWEB:Uploads"')
+    print("Gdrive Upload Complete!")
+
+divider()
+print("**** BDH-DL by JoyBangla ****")
+divider()
+MPD_URL = input("Enter NonDRM m3u8/Direct Stream URL: \n> ")
+download_drm_content(MPD_URL)
+drive_upload()
+divider()
+print("Process Finished. Final Video File is saved in /output directory.")
+divider()
+
+delete_choice = input("Delete cache files? (y/n)\ny) Yes (default)\nn) No\ny/n> ")
+
+if delete_choice == "n":
+    divider()
+else:
+    #empty_folder(TEMPORARY_PATH)
+    os.system(f'rm -r "{TEMPORARY_PATH}" && mkdir "{TEMPORARY_PATH}"')
